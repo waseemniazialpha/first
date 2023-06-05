1faae2ffc471425e7ce8eff9bfda77915779720e
waseemniazialpha <waseem.alphasquad@gmail.com>
2023-06-05 22:03:23 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,55 +1,127 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var myModal = document.getElementById("myModal");
+var myModalBlur = document.getElementById("myModalBlur");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function myFunction() {
+  myModal.style.display = "block";
+  myModalBlur.style.display = "block";
+}
+function hideFunction() {
+  myModal.style.display = "none";
+  myModalBlur.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function blurFunction(){
+  myModal.style.display = "none";
+  myModalBlur.style.display = "none";
+}
+// function myModalClick(){
+//   myModal.style.display ="block"
+//   myModalBlur.style.display = "block";
+// }
+
+
+// add member modal 
+
+var memberModal = document.getElementById("memberModal");
+var addMemberBlur = document.getElementById("addMemberBlur");
+
+
+function memFunction() {
+  memberModal.style.display = "block";
+  addMemberBlur.style.display = "block";
+}
+function memHideFunction() {
+  memberModal.style.display = "none";
+  addMemberBlur.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function memBlurFunction(){
+  memberModal.style.display = "none";
+  addMemberBlur.style.display = "none";
 }
+// add tag modal 
+
+var tagBlur = document.getElementById("tagBlur");
+var tagModal = document.getElementById("tagModal");
+
+
+function addTagFunction() {
+  tagModal.style.display = "block";
+  tagBlur.style.display = "block";
+}
+function tagHideFunction() {
+  tagModal.style.display = "none";
+  tagBlur.style.display = "none";
+}
+
+function tagBlurFunction(){
+  tagModal.style.display = "none";
+  tagBlur.style.display = "none";
+}
+
+
+
+
+
+
+var delModal = document.getElementById("delModal");
+var modalBlur = document.getElementById("modalBlur");
+
+function delFunction() {
+  delModal.style.display = "block";
+  modalBlur.style.display = "block";
+}
+function closeFunction() {
+  delModal.style.display = "none";
+  modalBlur.style.display = "none";
+}
+
+modalBlur.onclick = function () {
+  modalBlur.style.display = "none";
+  delModal.style.display = "none";
+}
+
+function cDelFunction(){
+  modalBlur.style.display = "none";
+  delModal.style.display = "none";
+}
+function cancelDelFunction(){
+  modalBlur.style.display = "none";
+  delModal.style.display = "none";
+}
+
+
+
+
 
 function ticketData(evt, ticketData) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(ticketData).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(ticketData).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 
-// parent hover 
+
+// parent hover
 
 
-$('nav-ul').hover(function(){
-  $(this).children('hoverli').css("fill","white");
-});
+// $('nav-ul').hover(function(){
+//   $(this).children('hoverli').css("fill","white");
+// });
